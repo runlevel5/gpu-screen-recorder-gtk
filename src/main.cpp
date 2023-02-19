ad5d5a3afd84a5ad2c3240a8e4396ebf07fcb009
dec05eba <dec05eba@protonmail.com>
2023-02-19 14:37:10 +0100

    Switch back from G_APPLICATION_DEFAULT_FLAGS to G_APPLICATION_FLAGS_NONE becauase flatpak gtk version is old

--- src/main.cpp
+++ src/main.cpp
@@ -2386,7 +2386,7 @@ static void activate(GtkApplication *app, gpointer userdata) {
 
 int main(int argc, char **argv) {
     setlocale(LC_ALL, "C");
-    GtkApplication *app = gtk_application_new("com.dec05eba.gpu_screen_recorder", G_APPLICATION_DEFAULT_FLAGS);
+    GtkApplication *app = gtk_application_new("com.dec05eba.gpu_screen_recorder", G_APPLICATION_FLAGS_NONE);
     g_signal_connect(app, "activate", G_CALLBACK(activate), nullptr);
     int status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
