93f6cb2a1b224235a6bc72c92e7912b3daf3983b
dec05eba <dec05eba@protonmail.com>
2023-05-17 18:49:39 +0200

    Add debug option to build script

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/sh -e
 
+opts="-O2 -g0 -DNDEBUG -Wall -Wextra -Werror -s"
+[ -n "$DEBUG" ] && opts="-O0 -g3 -Wall -Wextra -Werror";
+
 dependencies="gtk+-3.0 x11 xrandr libpulse"
 includes="$(pkg-config --cflags $dependencies)"
 libs="$(pkg-config --libs $dependencies) -ldl"
-gcc -c src/egl.c -O2 -g0 -DNDEBUG $includes
-g++ -c src/main.cpp -O2 -g0 -DNDEBUG $includes
-g++ -o gpu-screen-recorder-gtk -O2 egl.o main.o -s $libs
+gcc -c src/egl.c $opts $includes
+g++ -c src/main.cpp $opts $includes
+g++ -o gpu-screen-recorder-gtk -O2 egl.o main.o $libs $opts
