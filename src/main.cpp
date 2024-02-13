9f8db9c84a69e8271085ea71382bbb7d1977aaf2
dec05eba <dec05eba@protonmail.com>
2024-02-13 23:40:54 +0100

    Suggest installing mesa-extra if codecs missing

--- src/main.cpp
+++ src/main.cpp
@@ -3130,14 +3130,20 @@ static void load_config(const gpu_info &gpu_inf) {
     if(!supported_video_codecs.h264 && !supported_video_codecs.hevc && gpu_inf.vendor != GPU_VENDOR_NVIDIA && config.main_config.codec != "av1") {
         if(supported_video_codecs.av1) {
             GtkWidget *dialog = gtk_message_dialog_new_with_markup(GTK_WINDOW(window), GTK_DIALOG_MODAL, GTK_MESSAGE_WARNING, GTK_BUTTONS_OK,
-                "Your distro has disabled support for H264 and HEVC video codecs. Switched video codec to AV1. If you wish to use H264/HEVC video codecs then follow your distros guide to install a non-crippled mesa (with H264 and HEVC support) or switch to a less user hostile distro or recompile mesa from source. For example on fedora you may need to follow this: <a href=\"https://rpmfusion.org/Howto/Multimedia\">Hardware Accelerated Codec</a>.");
+                "Switched video codec to AV1 since H264/HEVC video encoding is either missing or disabled on your system. If your know that your system supports H264/HEVC video encoding then "
+                "try installing mesa-extra freedesktop runtime by running this command:\n"
+                "flatpak install --system org.freedesktop.Platform.GL.default//23.08-extra\n"
+                "and then restarting GPU Screen Recorder. If that doesn't work then you may have to install another mesa package for your distro.");
             gtk_dialog_run(GTK_DIALOG(dialog));
             gtk_widget_destroy(dialog);
             config.main_config.codec = "av1";
             gtk_combo_box_set_active_id(GTK_COMBO_BOX(video_codec_input_menu), config.main_config.codec.c_str());
         } else {
             GtkWidget *dialog = gtk_message_dialog_new_with_markup(GTK_WINDOW(window), GTK_DIALOG_MODAL, GTK_MESSAGE_ERROR, GTK_BUTTONS_OK,
-                "Either your GPU doesn't support H264/HEVC video codecs or your distro has disabled support for those video codecs. If you wish to use H264/HEVC video codecs then follow your distros guide to install a non-crippled mesa (with H264 and HEVC support) or switch to a less user hostile distro or recompile mesa from source, if you know that your GPU supports H264/HEVC. For example on fedora you may need to follow this: <a href=\"https://rpmfusion.org/Howto/Multimedia\">Hardware Accelerated Codec</a>.");
+                "H264/HEVC video encoding is either missing or disabled on your system. If your know that your system supports H264/HEVC video encoding then "
+                "try installing mesa-extra freedesktop runtime by running this command:\n"
+                "flatpak install --system org.freedesktop.Platform.GL.default//23.08-extra\n"
+                "and then restarting GPU Screen Recorder. If that doesn't work then you may have to install another mesa package for your distro.");
             gtk_dialog_run(GTK_DIALOG(dialog));
             gtk_widget_destroy(dialog);
             g_application_quit(G_APPLICATION(select_window_userdata.app));
