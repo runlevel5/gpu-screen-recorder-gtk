5710cb1662d02432c68ce287624b3eadaa122d76
dec05eba <dec05eba@protonmail.com>
2024-06-11 16:40:29 +0200

    Allowed icon name

--- src/main.cpp
+++ src/main.cpp
@@ -140,9 +140,9 @@ static double notification_timeout_seconds = 0.0;
 static double notification_start_seconds = 0.0;
 
 static AppIndicator *app_indicator;
-static const char *tray_idle_icon_name = "com.dec05eba.gpu_screen_recorder_tray_idle";
-static const char *tray_recording_icon_name = "com.dec05eba.gpu_screen_recorder_tray_recording";
-static const char *tray_paused_icon_name = "com.dec05eba.gpu_screen_recorder_tray_paused";
+static const char *tray_idle_icon_name = "com.dec05eba.gpu_screen_recorder.tray-idle";
+static const char *tray_recording_icon_name = "com.dec05eba.gpu_screen_recorder.tray-recording";
+static const char *tray_paused_icon_name = "com.dec05eba.gpu_screen_recorder.tray-paused";
 
 struct AudioInput {
     std::string name;
