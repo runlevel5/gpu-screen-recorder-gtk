c52c77212d98448eabbb455809e17dd3484ca4dd
dec05eba <dec05eba@protonmail.com>
2025-06-24 17:18:01 +0200

    Launch new ui immediately after switching to it

--- src/main.cpp
+++ src/main.cpp
@@ -2940,9 +2940,7 @@ static gboolean on_click_switch_to_new_ui(GtkButton*, gpointer) {
         gtk_widget_destroy(dialog);
     }
 
-    if(!service_install_successful)
-        launch_gsr_ui(false);
-
+    launch_gsr_ui(false);
     g_application_quit(G_APPLICATION(select_window_userdata.app));
     return true;
 }
